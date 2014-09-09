4ab3fb7c750e3a60da0b95f91bc73578d07aaa69
fainla <ainlapoiss@hot.ee>
2014-09-09 17:02:30 +0300

    final commit

--- index.php
+++ index.php
@@ -17,30 +17,34 @@
     echo $variable;
 ?>
 <h2>Stringid</h2>
-<?php echo "Hello world! <br> ";?>
-<?php echo 'Hello World!' ;?>
+<?php
+ 	echo "Hello world! <br> ";
+?>
+<?php
+	 echo 'Hello World!';
+?>
 
 <?php
     $number1 ="24";
     $number2 ="26";
     echo $number1 . $number2;
-    ?>
+?>
     <h2>Nimekirja väljastamine muutujate ja stringide abil</h2>
-   <?php
-   $student1 ="Syret";
-   $student2 ="Anneli";
-   $student3 ="Margit";
-   $student4 ="Eve";
-   $student5 ="Fränk";
+<?php
+   	$student1 ="Syret";
+   	$student2 ="Anneli";
+   	$student3 ="Margit";
+   	$student4 ="Eve";
+   	$student5 ="Fränk";
 
-   ?>
-   <?php
+?>
+<?php
    		echo "<li>" . $student1 . "</li>";
    		echo "<li>" . $student2 . "</li>";
    		echo "<li>" . $student3 . "</li>";
    		echo "<li>" . $student4 . "</li>";
    		echo "<li>" . $student5 . "</li>";
-   ?>
+?>
 <?php 
 
 	$firstName ="Fränk"; 
@@ -48,6 +52,23 @@
 	$fullName = $firstName . " " . $lastName;
 	echo "$fullName";
 ?>	
+	<br>
+
+<?php
+
+	$test = "Testmime";
+	echo "$test kas interpolatsioon töötab";
+	$age = "24";
+	echo "Ma olen $age-e aastane";
+?>
+
+<h2>Täisarvud</h2>
+<?php
+	$variable ="20";
+	$variable1 ="13";
+	echo ($variable * $variable1) * 2;
+?>
+
 </body>
 </html>
 
