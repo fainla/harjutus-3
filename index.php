fbd6a7425d3fc3b460359ef4f40eddf7931ee5db
fainla <ainlapoiss@hot.ee>
2014-09-09 15:40:00 +0300

    add sample variables

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+</head>
+<body>
+	<h1>Muutuja<h1>
+
+<?php
+    $variable = 10;
+    echo $variable;
+    echo "<br>";
+?>
+<?php
+    $variable = 20;
+    echo $variable;
+?>
+</body>
+</html>
+
+
